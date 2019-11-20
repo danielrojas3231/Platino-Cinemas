4df36a984806e43bc379336c28cfe3a1e13bc7d9
MetalPrime <53907237+MetalPrime@users.noreply.github.com>
2019-11-19 23:10:04 -0500

    Arreglo de Información de cambio en la Plantilla de Peliculas
    creo en la descripción es bien vergas

--- src/javascript/compra.js
+++ src/javascript/compra.js
@@ -1,9 +1,23 @@
-let banner = document.querySelector(".bannerMovie")
+let joker = document.querySelector("#1");
+let dedicada = document.querySelector("#2");
+let doctor = document.querySelector("#3");
+let terminator = document.querySelector("#4");
+let malefica = document.querySelector("#5");
+let infierno = document.querySelector("#6");
+let maldición = document.querySelector("#7");
+let huerfanos = document.querySelector("#8");
+let star = document.querySelector("#9");
+let sonic = document.querySelector("#10");
+let banner = document.querySelector(".bannerMovie");
+let poster = document.querySelector(".posterMovie");
+let title = document.querySelector(".titleMovie");
+let director = document.querySelector(".directorMovie");
+let casting = document.querySelector(".castingMovie");
+let sinopsis = document.querySelector(".sinopsisMovie");
 
 
 
 document.addEventListener("click",function(){
-
     banner.src="./../src/data/banner-doctor.jpg";
 
 })
